dad41840f02747b5e690bef3b0644425c5df868a
J. Nick Koston <nick@koston.org>
2023-06-15 14:55:34 -1000

    feat: add code

---
+++ src/kasa_crypt/kasa_crypt.py
@@ -0,0 +1,24 @@
+from struct import pack
+
+
+def encrypt(string: str) -> bytes:
+    """Encrypt."""
+    unencrypted = bytearray(string.encode())
+    key = 171
+    unencrypted_len = len(unencrypted)
+    encrypted = bytearray(unencrypted_len)
+    for idx, unencryptedbyte in enumerate(unencrypted):
+        key = key ^ unencryptedbyte
+        encrypted[idx] = key
+    return pack(">I", unencrypted_len) + encrypted
+
+
+def decrypt(string: bytes) -> str:
+    """Decrypt."""
+    key = 171
+    result = bytearray(string)
+    for idx, i in enumerate(string):
+        a = key ^ i
+        key = i
+        result[idx] = a
+    return result.decode()
