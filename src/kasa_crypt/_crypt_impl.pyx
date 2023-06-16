e7919f0104c98855e6af1010f07f00b038279b40
J. Nick Koston <nick@koston.org>
2023-06-15 18:05:10 -1000

    fix: add missing nulls

--- src/kasa_crypt/_crypt_impl.pyx
+++ src/kasa_crypt/_crypt_impl.pyx
@@ -8,14 +8,14 @@ cdef extern from "crypt_wrapper.h":
     void _decrypt_into(const char * encrypted, char * unencrypted)
 
 cdef char* _decrypt(const char *encrypted):
-    cdef char* unencrypted = <char *> malloc((len(encrypted)) * sizeof(char))
+    cdef char* unencrypted = <char *> malloc(sizeof(encrypted))
     if not unencrypted:
         return NULL  # malloc failed
     _decrypt_into(encrypted, unencrypted)
     return unencrypted
 
 cdef char* _encrypt(const char *unencrypted):
-    cdef char* encrypted = <char *> malloc((len(unencrypted)) * sizeof(char))
+    cdef char* encrypted = <char *> malloc(sizeof(unencrypted))
     if not encrypted:
         return NULL  # malloc failed
     _encrypt_into(unencrypted, encrypted)
--- src/kasa_crypt/crypt_wrapper.h
+++ src/kasa_crypt/crypt_wrapper.h
@@ -8,20 +8,24 @@
 void _encrypt_into(const char * unencrypted, char * encrypted) {
     uint8_t unencrypted_byte;
     uint8_t key = 171;
-    for(unsigned i = 0; i < strlen(unencrypted); i++) {
+    unsigned long len = strlen(unencrypted);
+    for(unsigned i = 0; i < len; i++) {
         unencrypted_byte = unencrypted[i];
         key = key ^ unencrypted_byte;
         encrypted[i] = key;
     }
+    encrypted[len] = '\0';
 }
 void _decrypt_into(const char * encrypted, char * unencrypted) {
     uint8_t unencrypted_byte;
     uint8_t encrypted_byte;
     uint8_t key = 171;
+    unsigned long len = strlen(encrypted);
     for(unsigned i = 0; i < strlen(encrypted); i++) {
         encrypted_byte = encrypted[i];
         unencrypted_byte = key ^ encrypted_byte;
         key = encrypted_byte;
         unencrypted[i] = unencrypted_byte;
     }
+    unencrypted[len] = '\0';
 }
