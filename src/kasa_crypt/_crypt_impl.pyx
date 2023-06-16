5e79680eaaeabeb08698937ba4c93b5c28029e7e
J. Nick Koston <nick@koston.org>
2023-06-15 18:19:58 -1000

    fix: malloc for null byte

--- src/kasa_crypt/_crypt_impl.pyx
+++ src/kasa_crypt/_crypt_impl.pyx
@@ -8,14 +8,14 @@ cdef extern from "crypt_wrapper.h":
     void _decrypt_into(const char * encrypted, char * unencrypted)
 
 cdef char* _decrypt(const char *encrypted):
-    cdef char* unencrypted = <char *> malloc(sizeof(encrypted))
+    cdef char* unencrypted = <char *> malloc(sizeof(encrypted) + 1)
     if not unencrypted:
         return NULL  # malloc failed
     _decrypt_into(encrypted, unencrypted)
     return unencrypted
 
 cdef char* _encrypt(const char *unencrypted):
-    cdef char* encrypted = <char *> malloc(sizeof(unencrypted))
+    cdef char* encrypted = <char *> malloc(sizeof(unencrypted) + 1)
     if not encrypted:
         return NULL  # malloc failed
     _encrypt_into(unencrypted, encrypted)
