6c66d4cfba3299d1b4e760ee141d7be36e9d18a9
J. Nick Koston <nick@koston.org>
2023-06-15 17:13:11 -1000

    fix: adjust

---
+++ src/kasa_crypt/python_impl.py
@@ -0,0 +1,21 @@
+def encrypt_pure_python(string: str) -> bytearray:
+    """Encrypt."""
+    unencrypted = string.encode()
+    key = 171
+    unencrypted_len = len(unencrypted)
+    encrypted = bytearray(unencrypted_len)
+    for idx, unencryptedbyte in enumerate(unencrypted):
+        key = key ^ unencryptedbyte
+        encrypted[idx] = key
+    return encrypted
+
+
+def decrypt_pure_python(string: bytes) -> str:
+    """Decrypt."""
+    key = 171
+    result = bytearray(len(string))
+    for idx, i in enumerate(string):
+        a = key ^ i
+        key = i
+        result[idx] = a
+    return result.decode()
