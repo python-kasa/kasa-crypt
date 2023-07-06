9bdf2cfdbe2bd56eefcf451f98e4289beec3f108
J. Nick Koston <nick@koston.org>
2023-07-05 15:01:24 -1000

    fix: do string length in python

--- src/kasa_crypt/_crypt_impl.pyx
+++ src/kasa_crypt/_crypt_impl.pyx
@@ -5,29 +5,25 @@ from libc.string cimport strlen
 
 
 cdef extern from "crypt_wrapper.h":
-    void _encrypt_into(const char * unencrypted, char * encrypted)
-    void _decrypt_into(const char * encrypted, char * unencrypted)
+    void _encrypt_into(const char * unencrypted, char * encrypted, unsigned long length)
+    void _decrypt_into(const char * encrypted, char * unencrypted, unsigned long length)
 
-cdef void _decrypt(const char *encrypted, char **unencrypted, Py_ssize_t *length):
-    cdef Py_ssize_t n = strlen(encrypted)
-    unencrypted[0] = <char *> malloc((n + 1) * sizeof(char))
+cdef void _decrypt(const char *encrypted, char **unencrypted, Py_ssize_t length):
+    unencrypted[0] = <char *> malloc((length + 1) * sizeof(char))
     if not unencrypted[0]:
         return  # malloc failed
-    _decrypt_into(encrypted, unencrypted[0] )
-    length[0] = n
+    _decrypt_into(encrypted, unencrypted[0], length)
 
-cdef void _encrypt(const char *unencrypted, char** encrypted, Py_ssize_t *length):
-    cdef Py_ssize_t n = strlen(unencrypted)
-    encrypted[0] = <char *> malloc((n + 1) * sizeof(char))
+cdef void _encrypt(const char *unencrypted, char** encrypted, Py_ssize_t length):
+    encrypted[0] = <char *> malloc((length + 1) * sizeof(char))
     if not encrypted[0]:
         return  # malloc failed
-    _encrypt_into(unencrypted, encrypted[0])
-    length[0] = n
+    _encrypt_into(unencrypted, encrypted[0], length)
 
 def encrypt(string: str) -> bytes:
     cdef char* encrypted = NULL
-    cdef Py_ssize_t length = 0
-    _encrypt(string.encode('utf-8'), &encrypted, &length)
+    cdef Py_ssize_t length = len(string)
+    _encrypt(string.encode('utf-8'), &encrypted, length)
     try:
         return encrypted[:length]
     finally:
@@ -35,8 +31,8 @@ def encrypt(string: str) -> bytes:
 
 def decrypt(string: bytes) -> str:
     cdef char* unencrypted = NULL
-    cdef Py_ssize_t length = 0
-    _decrypt(string, &unencrypted, &length)
+    cdef Py_ssize_t length = len(string)
+    _decrypt(string, &unencrypted, length)
     try:
         return unencrypted[:length].decode('utf-8')
     finally:
